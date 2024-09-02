cb7323d37fd0d83ced36ca5b35dfd0890ccfbf78
Alaa-Mostafaa <alan.mustafa.2002@gmail.com>
2024-09-02 23:37:33 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
-
-var menubutton=document.getElementsByClassName('menu');
-var uu=document.querySelector('ul');
-var call=document.getElementsByClassName('call-us')
-
+let menubutton=document.getElementsByClassName('menu');
+let uu=document.querySelector('ul');
+let call=document.getElementsByClassName('call-us')
 function ToggleMenu(){
-
-    var s = uu.getAttribute('style')
-
-    if( s == 'display:none'){
-
-        uu.setAttribute('style','display:flex')
-    
-    }else{
-        uu.setAttribute('style','display:none')
-    }
-}
+ let s = uu.getAttribute('style')
+ if( s == 'display:none'){uu.setAttribute('style','display:flex')}else{uu.setAttribute('style','display:none')}}
